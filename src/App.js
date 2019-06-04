c823e270ee99c226f3e96a371d0abf6acb1a4bb0
Tulio Viglione <tulio.l.viglione@gmail.com>
2019-06-04 20:40:38 -0300

    Primeiro componente

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import User from './User';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <User></User>
+        <User></User>       
       </header>
     </div>
   );
--- src/User.js
+++ src/User.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-
-function User() {
-  return (
-    <div>
-      Primeiro componente
-    </div>
-  );
+class User extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Primeiro componente</h1>
+            </div>
+        );
+    }
 }
-
 export default User;
