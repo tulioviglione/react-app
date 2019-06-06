43e9576e1bf0a6f78c132856ebf191c02f28df9b
Tulio Viglione <tulio.l.viglione@gmail.com>
2019-06-05 22:22:09 -0300

    Utilizacao de state e events

--- src/User.js
+++ src/User.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
 
 class User extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            name:"Nome"
+        }
+        this.alteraNome = this.alteraNome.bind(this);
+        this.resetaNome = this.resetaNome.bind(this);
+    }
+    
+    alteraNome(){
+        this.setState({
+            name:"Nome Alterado"
+        })
+    }
+
+    resetaNome(){
+        this.setState({
+            name:"Nome"
+        })
+    }
+
     render() {
         return(
             <div>
-                Nome: {this.props.name}
+                Nome enviado por parâmetro:{this.props.name}
+                <p></p>
+                Nome: {this.state.name}
+                <p></p>
+                <button onClick={this.alteraNome}>Altera Nome</button>
+                <button onClick={this.resetaNome}>Reseta Nome</button>
                 <p></p>
                 <img src = {this.props.photo} alt={this.props.name}></img>
+                
             </div>
         );
     }
